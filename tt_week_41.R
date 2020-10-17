9c47ac32d181ec6d1081c13ebc756769b7f812ad
Philip Parker <philipparker@MacBook-Pro.local>
2020-10-17 20:18:04 +1100

    Week 42 - slight fix

--- tt_week_41.R
+++ tt_week_41.R
@@ -9,6 +9,44 @@ library(lubridate)
 tuesdata <- tidytuesdayR::tt_load(2020, week = 41)
 extrafont::font_import("~/Library/Fonts/",prompt = FALSE)
 extrafont::fonts()
+# taken from https://themockup.blog/posts/2020-09-26-functions-and-themes-for-gt-tables
+gt_theme_538 <- function(data,...) {
+  data %>%
+    opt_all_caps()  %>%
+    opt_table_font(
+      font = list(
+        google_font("Arial Narrow"),
+        default_fonts()
+      )
+    ) %>%
+    tab_style(
+      style = cell_borders(
+        sides = "bottom", color = "transparent", weight = px(2)
+      ),
+      locations = cells_body(
+        columns = TRUE,
+        # This is a relatively sneaky way of changing the bottom border
+        # Regardless of data size
+        rows = nrow(data$`_data`)
+      )
+    )  %>% 
+    tab_options(
+      column_labels.background.color = "white",
+      table.border.top.width = px(3),
+      table.border.top.color = "transparent",
+      table.border.bottom.color = "transparent",
+      table.border.bottom.width = px(3),
+      column_labels.border.top.width = px(3),
+      column_labels.border.top.color = "transparent",
+      column_labels.border.bottom.width = px(3),
+      column_labels.border.bottom.color = "black",
+      data_row.padding = px(3),
+      source_notes.font.size = 12,
+      table.font.size = 16,
+      heading.align = "left",
+      ...
+    ) 
+}
 
 data <- tuesdata$tournament %>%
   filter(str_detect(school, "UConn")|str_detect(school, "Tennessee$"))
@@ -128,6 +166,6 @@ p3 <- ggdraw(p2) +
     tmp)
 
 p1 + p3
-ggsave(here("img", "week41.png"),width = 12, height = 6)
+ggsave(here("img", "week41.png"),width = 12, height = 6, dpi=300)
 
 
